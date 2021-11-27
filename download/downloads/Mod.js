d64d5182667bcba745963f47058cd05e570d472b
VNBPM on YT <77667370+iBLiSSIN@users.noreply.github.com>
2021-11-27 08:43:12 +0700

    Update Mod.js

--- download/downloads/Mod.js
+++ download/downloads/Mod.js
@@ -14,25 +14,6 @@
 (function(){
     'use strict';
 
-    var startoverlay = document.getElementById("start-overlay");
-var startrowheader = document.getElementById("start-row-header");
-
-startoverlay.style.backgroundImage = "url('https://static-cdn.jtvnw.net/jtv_user_pictures/67c7a3e6-1b7c-4bd3-97c5-953307470f70-profile_image-300x300.png')";
-startoverlay.style.backgroundColor = "black";
-
-startrowheader.style.backgroundImage = "url('')";
-
-var loop = setInterval(function(){
-    if(document.getElementsByClassName('ui-stats-table-content').size <= 2){
-       var stats = document.getElementsByClassName('ui-stats-table-row ui-stats-current')[0];
-	   var clone = stats.cloneNode(true);
-			var ui = document.getElementsByClassName('ui-stats-table-content')[0];
-			ui.parentNode.insertBefore(clone, ui);
-    }
-    }, 500);
-
-    document.getElementById("start-bottom-middle").innerHTML = "<a href='https://discord.gg/J8kBwk9' target='_blank'>Made by Philipp</a><a href='https://www.youtube.com/channel/UCRJIEWtHtxwql2KuUchPc2A' target='_blank'>YTIridium77 Mod</a>";
-
     var o = document.createElement("a");
 	o.setAttribute("id","my_Heart");
     o.style.fontSize = "25px";
@@ -415,66 +396,3 @@ let ping = new Date;
 };
 
 getPing();
-
-//custom background
-
-document.getElementById('modal-settings-body').innerHTML += '<h2>YTIridium77 Mod Menü</h2> <h2>Background Image</h2> <button class="backbutton">Iridium</button> <button class="backbutton">Tankalty YT</button> <button class="backbutton">GAMERIO</button> <button class="backbutton">Zmedo</button> <button class="backbutton">iWolf</button> <button class="backbutton">g0dak</button> <button class="backbutton">Jtrick</button> <button class="backbutton">JoLe</button>';
-
-
-function Iridium() {
-var startoverlay = document.getElementById("start-overlay"); startoverlay.style.backgroundImage = "url('https://static-cdn.jtvnw.net/jtv_user_pictures/67c7a3e6-1b7c-4bd3-97c5-953307470f70-profile_image-300x300.png')";
-startoverlay.style.backgroundSize = "auto";
-};
-function Tankalty() {
-    var startoverlay = document.getElementById("start-overlay"); startoverlay.style.backgroundImage = "url('https://cdn.discordapp.com/avatars/534413834455220224/670b7f91aaa4b54368cd6fafb54609c7.png?size=256')";
-    startoverlay.style.backgroundSize = "auto";
-};
-function Gamerio() {
-    var startoverlay = document.getElementById("start-overlay"); startoverlay.style.backgroundImage = "url('https://cdn.discordapp.com/avatars/527111830406037514/c43abb91cb654867b60edbc5f3ed99d3.png?size=256')";
-    startoverlay.style.backgroundSize = "auto";
-};
-function Zmedo() {
-    var startoverlay = document.getElementById("start-overlay"); startoverlay.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/749714154864246846/762309098557014066/-CC9A.jpeg')";
-    startoverlay.style.backgroundSize = "auto";
-};
-function iWolf() {
-    var startoverlay = document.getElementById("start-overlay"); startoverlay.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/749714154864246846/762309907344261150/image0.png')";
-    startoverlay.style.backgroundSize = "100%";
-    startoverlay.style.backgroundPositionY = "-175px";
-};
-function g0dak() {
-    var startoverlay = document.getElementById("start-overlay"); startoverlay.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/749712706210234448/762630592479100958/image0.gif')";
-    startoverlay.style.backgroundSize = "auto";
-};
-function Jtrick() {
-    var startoverlay = document.getElementById("start-overlay"); startoverlay.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/727538758055886849/763789074611830834/Screenshot_20201008-174406_Google.jpg')";
-    startoverlay.style.backgroundSize = "100%";
-};
-function JoLe() {
-    var startoverlay = document.getElementById("start-overlay"); startoverlay.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/759861463690379344/764867341418627092/ServerIcon_Not_Animated.png')";
-    startoverlay.style.backgroundSize = "auto";
-};
-function Colorchange(element){
-    element.style.color = "white";
-};
-
-var backbutton = document.getElementsByClassName('backbutton');
-backbutton[0].addEventListener("click", Iridium);
-backbutton[1].addEventListener("click", Tankalty);
-backbutton[2].addEventListener("click", Gamerio);
-backbutton[3].addEventListener("click", Zmedo);
-backbutton[4].addEventListener("click", iWolf);
-backbutton[5].addEventListener("click", g0dak);
-backbutton[6].addEventListener("click", Jtrick);
-backbutton[7].addEventListener("click", JoLe);
-
-var i;
-
-for(i = 0; i <= backbutton.length; i++){
-backbutton[i].style.backgroundColor = "green";
-backbutton[i].style.margin = "5px";
-backbutton[i].style.borderColor = "white";
-backbutton[i].style.borderRadius = "5px";
-};
-
-})();
